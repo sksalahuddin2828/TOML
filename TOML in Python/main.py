2cb2f293bd4434fa5175932be4603e465ec177e4
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-06-23 19:38:57 +0600

    Update main.py

--- TOML in Python/main.py
+++ TOML in Python/main.py
@@ -5,7 +5,7 @@ def load_toml() -> dict:
     """Load TOML data from file"""
 
     # rb = read bytes mode
-    # with open("config.toml", "rb") as f:
+    # Must change your path or use this --> with open("config.toml", "rb") as f:
     with open("C:/Users/CBZ/Desktop/TOML/config.toml", "rb") as f:
         toml_data: dict = tomllib.load(f)
         return toml_data
@@ -14,6 +14,25 @@ if __name__ == '__main__':
     data: dict = load_toml()
     pprint(data, sort_dicts=False)
 
+
+
+#-----------------------------------------------------------------------------------------------------------
+
+
+# Answer: {'name': 'My Project',
+ 'version': '1.0.1',
+ 'website': 'https://www.2nonenone2.com/',
+ 'items': {'numbers': [1, 2, 3],
+           'letters': ['a', 'b', 'c'],
+           'details': {'updated': True,
+                       'author': 'Test Name',
+                       'timestamp': datetime.datetime(2023, 6, 6, 0, 0, tzinfo=datetime.timezone.utc)}},
+ 'database': {'file': {'type': 'sqlite', 'path': 'data.db'}},
+ 'inline_table': {'inline': {'name': 'Test Name', 'age': 30}},
+ 'table_group': [{'fruit': 'apple'}, {'fruit': 'orange'}, {'fruit': 'banana'}]}
+
+
+#-----------------------------------------------------------------------------------------------------------
 
 """
 # Write initial TOML data to the file if needed
